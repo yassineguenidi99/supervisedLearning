5fb92bba4936a026bee68388a4becd4412d9d2ee
yassine guenidi <94716214+yassineguenidi99@users.noreply.github.com>
2022-07-04 17:58:33 +0100

    Create supervised.py

---
+++ supervised.py
@@ -0,0 +1,54 @@
+# Create X and y arrays
+X = sales_df.drop("sales", axis=1).values
+y = sales_df["sales"].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Instantiate the model
+reg = LinearRegression()
+
+# Fit the model to the data
+reg.fit(X_train, y_train)
+
+# Make predictions
+y_pred = reg.predict(X_test)
+print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
+********************
+# Import mean_squared_error
+from sklearn.metrics import mean_squared_error
+
+# Compute R-squared
+r_squared = reg.score(X_test, y_test)
+
+# Compute RMSE
+rmse = mean_squared_error(y_test, y_pred, squared=False)
+
+# Print the metrics
+print("R^2: {}".format(r_squared))
+print("RMSE: {}".format(rmse))
+****************
+cross validation
+****************
+# Import the necessary modules
+from sklearn.model_selection import KFold, cross_val_score
+
+# Create a KFold object
+kf = KFold(n_splits=6, shuffle=True, random_state=5)
+
+reg = LinearRegression()
+
+# Compute 6-fold cross-validation scores
+cv_scores = cross_val_score(reg, X, y, cv=kf)
+
+# Print scores
+print(cv_scores)
+**************************
+# Print the mean
+print(np.mean(cv_results))
+
+# Print the standard deviation
+print(np.std(cv_results))
+
+# Print the 95% confidence interval
+print(np.quantile(cv_results, [0.025, 0.975]))
+*****************
